a5dd403d3789c649fb70b4759a23eaa883dab31f
unknown <pranesh.m2007@gmail.com>
2025-02-24 15:32:11 +0530

    completed

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -6,13 +6,11 @@ public class App {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        // int a = scan.nextInt();
-        // int b = scan.nextInt();
-        // int c = scan.nextInt();
-        // scan.close();
-        int a = 1;
-        int b = 2;
-        int c = 3;
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+       
 
         System.out.println(a);
         System.out.println(b);
